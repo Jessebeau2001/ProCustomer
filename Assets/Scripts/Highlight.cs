0ba336187be42b612da1d70447e7e3fe03b3a573
Jessebeau2001 <jessegamen@gmail.com>
2020-09-07 15:25:17 +0200

    Shader Swapping

---
+++ Assets/Scripts/Highlight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+    public Shader shader;
+    Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+		{
+            mat.shader = shader;
+		}
+    }
+}
