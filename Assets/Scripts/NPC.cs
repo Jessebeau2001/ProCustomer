45dd125d231fb7f6f0dae41e4df18a29c22862d7
Jessebeau2001 <jessegamen@gmail.com>
2020-09-10 15:05:03 +0200

    Added navigation + TMP vvv
    + Created Navigation Script
    + Imported TextMeshPro

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void SetDest(Vector3 dest) {
+        agent.SetDestination(dest);
+    }
+}
