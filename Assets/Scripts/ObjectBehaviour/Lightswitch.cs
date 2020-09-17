050e817f20ddf70c89356cce54ef3d7322917c98
Jessebeau2001 <jessegamen@gmail.com>
2020-09-17 16:16:08 +0200

    Cleaned LookingAtRecognition.cs
    + Finished Lightswitch and some other convieniences

---
+++ Assets/Scripts/ObjectBehaviour/Lightswitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour
+{
+    public bool IsOn;
+    public Light lightSource;
+    [SerializeField] Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+        UpdateState();
+    }
+
+    public void SwapState() {
+        IsOn = !IsOn;
+        UpdateState();
+    }
+
+    public void UpdateState() {
+        lightSource.enabled = IsOn;
+        anim.SetBool("IsOn", IsOn);
+    }
+}
