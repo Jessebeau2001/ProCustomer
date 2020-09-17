405bd4e6edc8e79e741f16e1d6b726190a9b22ea
Kristýna Pavlátová <60941311+KristynaPavlatova@users.noreply.github.com>
2020-09-17 13:33:06 +0200

    AudioManger + game background music added
    AudioManager created
    Playing the background music (rain) when entering the MainScene

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource gameMusic;
+    public AudioSource doorKnob;
+
+    //checking if music is already playing
+    private bool isGameMusicPlaying = false;
+
+    //public getter-------------------------------------------------------------
+    //singleton, we can access this game manager by this method
+    //--------------------------------------------------------------------------
+    public static AudioManager GetManager()
+    {
+        return currentAudioManager;
+    }
+    static AudioManager currentAudioManager = null;//making sure there is always only one game manager in the scene
+
+    //--------------------------------------------------------------------------
+    //ONLY ONE GAME MANAGER IN THE GAME
+    //--------------------------------------------------------------------------
+    private void Awake()
+    {
+        //if there is no GameManager yet
+        if (currentAudioManager == null)
+        {
+            currentAudioManager = this; //this is the current (and only one) GameManager
+            DontDestroyOnLoad(gameObject);
+
+            //listen to events to play audioSources
+            LookingAtRecognition.playAudioDoorKnob += playDoorKnob;
+        }
+        else
+        {
+            Destroy(gameObject); //otherwise destroy this game object to prevent having multiple GameManagers
+        }
+    }
+    private void OnDestroy()
+    {
+        if (currentAudioManager == this)//if this is the current game manager
+        {
+            currentAudioManager = null;//set it to null on destroy
+
+            LookingAtRecognition.playAudioDoorKnob -= playDoorKnob;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Game music-----------------------------------------
+        if(SceneManager.GetActiveScene().name == "MainScene")
+        {
+            if (!isGameMusicPlaying)
+            {
+                gameMusic.Play();
+                isGameMusicPlaying = true;
+            }
+        }
+    }
+
+    private void playDoorKnob()
+    {
+        doorKnob.Play();
+    }
+}
